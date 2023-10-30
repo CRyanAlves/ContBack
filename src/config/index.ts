75b15a340fe9b149098eed29957dc3e8bd0b157b
unknown <35073@fiecalunos.lan>
2023-10-30 19:27:35 -0300

    fix-path

--- src/config/index.ts
+++ src/config/index.ts
@@ -7,10 +7,12 @@ import authRouter from '@modules/User/routes/auth.router';
 import diaryRouter from '@modules/Diary/routes/diary.router';
 import sosRouter from '@modules/SOS/routes/sosUpload.router';
 import briefRouter from '@modules/Brief/routes/brief.router';
+import * as path from 'path';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'pictures')));
 
 app.use(morgan('combined'));
 
